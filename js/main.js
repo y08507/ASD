80d1c5434061c6cf8d535c47b6ff07a06a6cc9df
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-05-08 19:17:00 -0400

    Adding jQuery

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@
  * Term: 1305
  * boilerplate Activity Week 1
  */
+
+$('#pageOne').on('pageinit', function(){
+    //code needed for home page goes here
+
+});
+
+$('#addItem').on('pageinit', function () {
+
+});
